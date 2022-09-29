5a0421232025fd9fc2784ad06bf4e275125eb222
mostofashawon <mostofashawon.xavi@gmail.com>
2022-09-29 00:25:33 -0700

    details screeen working ....

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:project/controller/signup_controller.dart';
+import 'package:project/widget/customText.dart';
+import 'package:get/get.dart';
+import 'package:project/widget/custom_edit_text.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +12,200 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home:  MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
-  final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    SignUpController controller = Get.put(SignUpController());
+
+    List<String> genderList = ["Male","Female"];
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Obx(() => Column(
+        children: <Widget>[
+
+          getCustomText("Sign Up",30.0),
+
+          SizedBox(height: screenHeight * 0.030,),
+
+          Form(
+            key: controller.loginFormKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.065,
+                ),
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      left: screenHeight * 0.019,
+                      right: screenHeight * 0.019,
+                    ),
+                    child: getCustomEditText(controller, 'Enter EmailAddress', 'email',Icons.email),
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.025,
+                ),
+
+                // Padding(
+                //   padding: EdgeInsets.only(
+                //       left: screenHeight * 0.019,
+                //       right: screenHeight * 0.019
+                //   ),
+                //   child: Container(
+                //     height: screenHeight * 0.075,
+                //     width: double.infinity,
+                //     color: Colors.grey,
+                //     child: Padding(
+                //         padding: EdgeInsets.only(
+                //             left: screenHeight * 0.019,
+                //             right: screenHeight *0.019
+                //         ),
+                //         child:  DropdownButton(
+                //
+                //
+                //           value: controller.dropdownvalue,
+                //
+                //
+                //           icon:Icon(Icons.keyboard_arrow_down),
+                //
+                //
+                //           items: controller.items.map((String items) {
+                //             return DropdownMenuItem(
+                //               value: items,
+                //               child: Text(items),
+                //             );
+                //           }).toList(),
+                //           onChanged: (newValue) {
+                //             // controller.dropdownvalue.value = newValue.toString();
+                //           },
+                //         )
+                //     ),
+                //   ),
+                // ),
+                SizedBox(height: screenHeight * 0.015,),
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: screenHeight * 0.019,
+                      right: screenHeight * 0.019
+                  ),
+                  child: Container(
+                    height: screenHeight * 0.075,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black)
+                    ),
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(
+                            left: screenHeight * 0.019,
+                          ),
+                          child: GestureDetector(
+                            child: Icon(Icons.calendar_month),
+                            onTap: (){
+                              controller.chooseExpireDate();
+                            },
+                          ),
+                        ),
+                        SizedBox(height: screenHeight * 0.015,),
+                        Padding(
+                            padding: EdgeInsets.only(
+                              left: screenHeight * 0.019,
+                            ),
+                            child: Obx(() =>
+                                Text(
+                                  controller.selectedDate.value.toString(),
+                                  style: TextStyle(
+                                      fontSize: screenHeight * 0.020,
+                                      color: Colors.black
+                                  ),
+                                ),
+                            )
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.015,),
+                Padding(
+                  padding: EdgeInsets.only(
+                    left: screenHeight * 0.019,
+                    right: screenHeight * 0.019,
+                  ),
+                  child: getCustomEditText(controller, 'Enter password', 'pass',Icons.key),
+                ),
+                SizedBox(height: screenHeight * 0.035,),
+                Padding(
+                    padding: EdgeInsets.only(
+                      left: screenHeight * 0.019,
+                      right: screenHeight * 0.019,
+                    ),
+                    child: getCustomEditText(controller, 'Enter confirmPassword', 'pass',Icons.key),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.025,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if(controller.password.value == controller.confirmPass.value ){
+
+                      }
+
+                      else {
+                          Get.snackbar('Status', 'Validation Error');
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(primary: Colors.grey),
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                          color: Colors.black,
+                          letterSpacing: 2,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w700),
+                    ))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+
+          Visibility(
+              visible: false,
+              child: Text(controller.selectedDate.value))
+
+        ],
+      )) ,
     );
   }
 }
